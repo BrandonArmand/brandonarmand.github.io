9743ca8811d930847b2b486e6fa374620612207b
Brandon Welsh <brandon@MacBook-Pro-2.local>
2018-03-25 04:35:39 -0700

    cleaner projects, and animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     $(".projects").hide();
+    
     $(".buttonProject").click(function(){
         $("#about").hide(0,function(){
           $(".projects").slideDown(1000);
         });
     });
-    
+
     $(".buttonAbout").click(function(){
         $(".projects").slideUp(1000, function(){
           $("#about").show();

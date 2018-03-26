f619ac6c5091f3856c6f515d7537aa0733012de3
Brandon Welsh <brandon@MacBook-Pro-2.local>
2018-03-26 04:09:23 -0700

    styling, optimizing links

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
     $(".projects").hide();
-    
+
     $(".buttonProject").click(function(){
         $("#about").hide(0,function(){
-          $(".projects").slideDown(1000);
+          $(".projects").slideDown(1000, function(){
+            $('html, body').animate({
+              scrollTop: ($('#projects').offset().top)
+          },500);
+          });
         });
     });
 
     $(".buttonAbout").click(function(){
         $(".projects").slideUp(1000, function(){
-          $("#about").show();
+          $("#about").show(0, function(){
+            $('html, body').animate({
+              scrollTop: ($('#about').offset().top)
+          },500);
+          });
         });
     });
 });

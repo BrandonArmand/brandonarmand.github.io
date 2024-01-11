1af5f7c677ecdc75f2d31f6114a47068e1f1bdab
Brandon A. Welsh <contact@brandonarmand.com>
2024-01-10 20:49:37 -0500

    what have i done?

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
+var cmdHistory = []
+var cmdHistoryIndex = 0
 $(document).ready(function(){
     var info = $('.my-info').clone()
-    // remove the class 'delay-init' from the child div of info
     info.children().removeClass('delay-init')
+
+    cmdHistory = []
     $(".projects").hide();
 
     $(".buttonProject").click(function(){
@@ -196,11 +199,19 @@ $(document).ready(function(){
       case 8:
         $('.input').html($('.input').html().slice(0, -1))
         break;
+      // up arrow
       case 13:
+        cmdHistoryIndex = 0;
+        cmdHistory.push($('.input').html())
         $('.stack').append('<h3 class="commands display-4">$ ' + $('.input').html() + '</h3>')
         var response = $('<h5 class="my-info"></h5>')
         if($('.input').html().includes('node&nbsp;welcome.js')){
           $('.stack').append(info.clone());
+        }
+        if($('.input').html().includes('history')){
+          for(var i = 0; i < cmdHistory.length; i++){
+            $('.stack').append($(`<p class="code-snippet" style="margin-bottom: 10px; margin-left: 20px;">${cmdHistory[i]}</p><br/>`))
+          }
         }
         else if($('.input').html() == 'clear' || $('.input').html() == 'cls' ){
           $('.stack').html('')
@@ -226,12 +237,24 @@ $(document).ready(function(){
           $('.stack').append($(`<h5 class="my-info">${loc} not found</h5>`))
         }
         else if($('.input').html().includes('help')){
+          $('.stack').append($('<h5 class="code-snippet" style="display: inline-flex; margin-left: 20px;"><span class="fa-solid fa-caret-up" style="color: whitesmoke;"></span></h5> / <h5 class="code-snippet" style="display: inline-flex;"><span class="fa-solid fa-caret-down" style="color: whitesmoke;"></span></h5><br/><h5 class="my-info"><span style="margin-left:25%; white-space: nowrap;">Navigate up and down through the command history.</span></h5>'))
           $('.stack').append($('<h5 class="my-info"><span class="code-snippet">ls</span>            <br/><span style="margin-left:25%; white-space: nowrap;">Search nearby paths (pages).</span></h5>'))
           $('.stack').append($('<h5 class="my-info"><span class="code-snippet">cd</span>            <br/><span style="margin-left:25%; white-space: nowrap;">Move towards next path (page).</span></h5>'))
           $('.stack').append($('<h5 class="my-info"><span class="code-snippet">node</span>          <br/><span style="margin-left:25%; white-space: nowrap;">Run (fake) Node script.</span></h5>'))
           $('.stack').append($('<h5 class="my-info"><span class="code-snippet">cls</span> / <span class="code-snippet">clear</span>   <br/><span style="margin-left:25%; white-space: nowrap;">Clear terminal.</span></h5>'))
           $('.stack').append($('<h5 class="my-info"><span class="code-snippet">help</span>          <br/><span style="margin-left:25%; white-space: nowrap;">This.</span></h5>'))
-
+          $('.stack').append($('<h5 class="my-info"><span class="code-snippet">history</span>       <br/><span style="margin-left:25%; white-space: nowrap;">Show command history.</span></h5>'))
+          $('.stack').append($('<h5 class="my-info"><span class="code-snippet">why</span>       <br/><span style="margin-left:25%; white-space: nowrap;">Why I built this terminal.</span></h5>'))
+          $('.stack').append($('<h5 class="my-info"><span class="code-snippet">beep boop</span>       <br/><span style="margin-left:25%; white-space: nowrap;">🤔</span></h5>'))
+        }
+        else if ($('.input').html().includes('why')){
+          $('.stack').append($('<h5 class="my-info"><span class="text-highlight" style="color: orange">Why</span> have I built this terminal?</h5>'))
+          $('.stack').append($('<h5 class="my-info">I honest to God have <span class="text-highlight loud-text" style="color: whitesmoke">no idea</span> why. It has been fun to build, but the code is ancient, and the longer I iterate, the messier it becomes lol.</h5>'))
+          $('.stack').append($('<h5 class="my-info">The complexity of it\'s functionality is impressive though!</h5>'))
+          $('.stack').append($('<span class="my-info" style="whitespace: nowrap;"><span style="color: #bedbf3">└</span>[∵<span style="color: #bedbf3">┌</span>]<span style="color: #b0e6b8;">└</span>[ ∵ ]<span style="color: #b0e6b8;">┘</span>[<span style="color: orange">┐</span>∵]<span style="color: orange">┘</span></span>'))
+        }
+        else if ($('.input').html().includes('beep&nbsp;boop')){
+          $('.stack').append($('<span class="my-info" style="whitespace: nowrap;"><span style="color: #bedbf3">└</span>[∵<span style="color: #bedbf3">┌</span>]<span style="color: #b0e6b8;">└</span>[ ∵ ]<span style="color: #b0e6b8;">┘</span>[<span style="color: orange">┐</span>∵]<span style="color: orange">┘</span></span>'))
         }
         else {
           let command = $('.input').html();
